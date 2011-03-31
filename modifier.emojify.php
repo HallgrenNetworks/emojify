9888cfb9c825bddb5f8a8dcab556289bf439d70c
Chris Hallgren <challgren@hallgrennetwork.com>
2011-03-31 03:11:07 -0500

    Initial Import

---
+++ modifier.emojify.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * File:     modifier.emojify.php
+ * Type:     modifier
+ * Name:     emojify
+ * Purpose:  Turns a UTF8 string into emoji images
+ * -------------------------------------------------------------
+ */
+function smarty_modifier_emojify($string) {
+    return preg_replace("/([\\xC0-\\xF7]{1,1}[\\x80-\\xBF]+)/e", '_utf8_to_emojiimages("\\1")', $string);
+}
+
+function _utf8_to_emojiimages($data) {
+    $ret = 0;
+    foreach((str_split(strrev(chr((ord($data{0}) % 252 % 248 % 240 % 224 % 192) + 128) . substr($data, 1)))) as $k => $v)
+        $ret += (ord($v) % 128) * pow(64, $k);
+    return '<img src="/images/emoji/' . dechex((int)$ret) .'.png" alt="emoji" class="emoji" />';
+}
+?>
