2ab07da9a6e032bc1faaaa62133a66e516e0f182
Chris Hallgren <challgren@hallgrennetwork.com>
2012-03-05 22:45:24 -0600

    Updating code adding javascript support

--- README.txt
+++ README.txt
@@ -1,27 +1,21 @@
-Welcome to php-emojify
+Welcome to emojify
+Thie will allow you to place iOS, iPhone, iPad, and iPod emoji icons on your web site so all users can see
+in every browser and every platform. No more square boxes
 
-To setup use the following example
+If you've gotten this far in your quest to enable emoji support you've come to the right place. @webguyatwork and @challgren have reverse engineered
+the new iOS5 emoji and created a method to replace them in your web site. We've included 4 way to accomplish this task but you must first follow a couple steps.
 
 
-Create a php file called index.php
-
-<?php
-include("emojify.php");
-
-echo emojify("String in UTF8 format with emoji characters");
-?>
-
-Upload /emoji to /images/emoji on your server or change the url in emojify.php or modifier.emojify.php 
-
+1: Upload the folder /emoji to /images/emoji on your server or change the url in emojify.php, emojify.js, emojify.jquery.js, and modifier.emojify.php 
 
 If you use Smarty template engine you still need to upload the image directory. But just upload modifier.emojify.php to your plugin directory and simply use {$string|emojify}
-
 
 If your not sure what your plugin directory is read the following doc please http://www.smarty.net/docsv2/en/variable.plugins.dir.tpl
 
 NOTE: All Strings sent to the emojify command must be utf8mb4 to support iOS5 emoji
 
-If your storing the data in a database make sure you collation is set to utf8mb4_general_ci to support iOS5 emoji that are Unicode 6.0
+If your storing the data in a database make sure you collation is set to utf8mb4_general_ci to support iOS5 emoji that are Unicode 6.1. This support is only in mySQL 5.5 and above.
 
 *** Known Issues ***
 Due to a change with iOS5 flags are not support just yet.
+The RegEx for the javascript is a little to open on its range once I reverse engineer the pattern I'll update the git repo
---
+++ emojify.js
@@ -0,0 +1,53 @@
+function emojify(stringInput) {
+	var replaceRegex = new RegExp(/([\u0080-\uFFFF]{1,1}[\u0080-\uFFFF])/g);
+	return stringInput.replace(replaceRegex, emoji_replacer);	
+}
+
+function emoji_replacer(str, p1) {
+	return decimalToHex(ord(p1.toString(16))).toString().toLowerCase().replace(
+	/^([\da-f]+)$/i,
+	'<img src="/images/emoji/$1.png" alt="emoji" />'
+	);
+}
+
+function decimalToHex(d, padding) {
+    var hex = Number(d).toString(16);
+    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+        hex = "0" + hex;
+    }
+
+    return hex;
+}
+
+function ord(string) {
+    // Returns the codepoint value of a character  
+    // 
+    // version: 1109.2015
+    // discuss at: http://phpjs.org/functions/ord
+    // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+    // +   bugfixed by: Onno Marsman
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // +   input by: incidence
+    // *     example 1: ord('K');
+    // *     returns 1: 75
+    // *     example 2: ord('\uD800\uDC00'); // surrogate pair to create a single Unicode character
+    // *     returns 2: 65536
+    var str = string + '',
+        code = str.charCodeAt(0);
+    if (0xD800 <= code && code <= 0xDBFF) { // High surrogate (could change last hex to 0xDB7F to treat high private surrogates as single characters)
+        var hi = code;
+        if (str.length === 1) {
+            return code; // This is just a high surrogate with no following low surrogate, so we return its value;
+            // we could also throw an error as it is not a complete character, but someone may want to know
+        }
+        var low = str.charCodeAt(1);
+        return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
+    }
+    if (0xDC00 <= code && code <= 0xDFFF) { // Low surrogate
+        return code; // This is just a low surrogate with no preceding high surrogate, so we return its value;
+        // we could also throw an error as it is not a complete character, but someone may want to know
+    }
+    return code;
+}
