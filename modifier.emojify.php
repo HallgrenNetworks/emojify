87a85ba9bc3c787a769041e70b7baf1d3e209582
Chris Hallgren <challgren@hallgrennetwork.com>
2014-10-07 09:30:52 -0500

    Updating for php 5.5

--- modifier.emojify.php
+++ modifier.emojify.php
@@ -9,10 +9,11 @@
  * -------------------------------------------------------------
  */
 function smarty_modifier_emojify($string) {
-    return preg_replace("/([\\xC0-\\xF7]{1,1}[\\x80-\\xBF]+)/e", '_utf8_to_emojiimages("\\1")', $string);
+    return preg_replace_callback("/([\\xC0-\\xF7]{1,1}[\\x80-\\xBF]+)/", "_utf8_to_emojiimages", $string);
 }
 
 function _utf8_to_emojiimages($data) {
+    $data = $matches[0];
     $ret = 0;
     foreach((str_split(strrev(chr((ord($data{0}) % 252 % 248 % 240 % 224 % 192) + 128) . substr($data, 1)))) as $k => $v)
         $ret += (ord($v) % 128) * pow(64, $k);
